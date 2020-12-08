07c986c03fa37f2f37c2683442a3b4dea57c2291
KlsMrn <75686910+KlsMrn@users.noreply.github.com>
2020-12-08 23:45:28 +0300

    Add files via upload

---
+++ src/mapping.ts
@@ -0,0 +1,33 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { BoltToken, Approval, Transfer } from "../generated/BoltToken/BoltToken"
+import { _Approval, _Transfer } from "../generated/schema"
+
+export function handleApproval(event: Approval): void {
+  let entity = _Approval.load(event.params.owner.toHex())
+
+  if (entity == null) {
+    entity = new _Approval(event.params.owner.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+  entity.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  let entity = _Transfer.load(event.params.from.toHex())
+
+  if (entity == null) {
+    entity = new _Transfer(event.params.from.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+  entity.save()
+}
